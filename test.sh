48d78b998abda035aae0c81cc887197d856c27e5
zzp1012 <zzp1012@sjtu.edu.cn>
2020-07-07 23:32:59 +0800

    update-3

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 make memcheck
 mkdir outputs
+
 ./test_1.out > ./outputs/test_1.out
 ./test_2.out > ./outputs/test_2.out
 ./test_3.out > ./outputs/test_3.out
 ./test_4.out > ./outputs/test_4.out
 ./test_5.out > ./outputs/test_5.out
+
+dos2unix ./outputs/*.out ./outputs/*.ans
 
 diff ./outputs/test_1.out ./outputs/test_1.ans
 diff ./outputs/test_2.out ./outputs/test_2.ans
@@ -12,5 +15,5 @@ diff ./outputs/test_3.out ./outputs/test_3.ans
 diff ./outputs/test_4.out ./outputs/test_4.ans
 diff ./outputs/test_5.out ./outputs/test_5.ans
 
-#rm -rf ./outputs/*.out
+rm -rf ./outputs/*.out
 make clean
